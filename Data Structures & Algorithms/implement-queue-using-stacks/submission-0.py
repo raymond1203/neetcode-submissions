5cf9e3a4dcf24b14a9491a1d98a2730b49d65d98
OZ <77887324+raymond1203@users.noreply.github.com>
2026-09-20 20:22:43 +0900

    Add: implement-queue-using-stacks - submission-0

---
+++ Data Structures & Algorithms/implement-queue-using-stacks/submission-0.py
@@ -0,0 +1,23 @@
+class MyQueue:
+    def __init__(self):
+        self.in_stack = []
+        self.out_stack = []
+
+    def push(self, x: int) -> None:
+        self.in_stack.append(x)
+
+    def pop(self) -> int:
+        self._transfer_if_needed()
+        return self.out_stack.pop()
+
+    def peek(self) -> int:
+        self._transfer_if_needed()
+        return self.out_stack[-1]
+
+    def empty(self) -> bool:
+        return not self.in_stack and not self.out_stack
+
+    def _transfer_if_needed(self) -> None:
+        if not self.out_stack:
+            while self.in_stack:
+                self.out_stack.append(self.in_stack.pop())
